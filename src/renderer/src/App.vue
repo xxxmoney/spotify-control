a7d7fc2e126a5970d6a1436e1d810742ace5ed8a
xxxmoney <xxxmoney111@gmail.com>
2025-04-28 21:43:32 +0200

    UI improvements

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -1,26 +1,50 @@
 <script setup lang="ts">
 import {useElectronAPI} from "@renderer/composables/api.comp";
+import {onMounted, ref} from "vue";
+import * as HID from "node-hid";
 
 const api = useElectronAPI();
 
-const handle = async () => {
-  const result = await api.getDevices();
-  console.log(result);
+const devices = ref([] as HID.Device[]);
+
+const getDevices = async () => {
+  devices.value = [];
+  devices.value = await api.getDevices();
 };
+
+const showDevice = async (device: HID.Device) => {
+  // TODO
+};
+
+onMounted(async () => {
+  await getDevices();
+});
 </script>
 
 <template>
   <img alt="logo" class="logo" src="./assets/electron.svg" />
   <div class="creator">Created by <code>xxxmoney</code></div>
   <div class="text">
-    Dummy text
+    Spotify Control
   </div>
   <p class="tip">
-    Dummy tip text
+    Select device:
   </p>
-  <div class="actions">
-      <button class="action" @click="handle">
-        Get devices
+
+  <button class="action" @click="getDevices">
+    Refresh
+  </button>
+
+  <div class="devices">
+    <div :key="device.productId" v-for="device in devices" class="device">
+      <button class="action" @click="showDevice(device)">
+        {{device.manufacturer}} {{ device.product }}
       </button>
+    </div>
   </div>
+<!--  <div class="actions">-->
+<!--      <button class="action" @click="handle">-->
+<!--        Get devices-->
+<!--      </button>-->
+<!--  </div>-->
 </template>
