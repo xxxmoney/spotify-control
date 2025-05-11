c5b5517639107d54008f9313ce603de614de0133
xxxmoney <xxxmoney111@gmail.com>
2025-05-11 17:19:05 +0200

    UI improvements

--- src/renderer/src/layouts/MainLayout.vue
+++ src/renderer/src/layouts/MainLayout.vue
@@ -1,10 +1,16 @@
 <script setup lang="ts">
 import NavBar from '@renderer/components/layout/NavBar.vue'
+import { useDeviceStore } from '@renderer/stores/device.store'
+import { computed } from 'vue'
+
+const store = useDeviceStore()
+
+const isRunning = computed(() => store.isRunning)
 </script>
 
 <template>
   <header>
-    <NavBar />
+    <NavBar v-if="!isRunning" />
   </header>
 
   <main>
