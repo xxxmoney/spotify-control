ba9ef9aa3c86859d4c425cfa52038e762b3e33e0
xxxmoney <xxxmoney111@gmail.com>
2025-07-07 19:28:56 +0200

    Axes

---
+++ src/renderer/src/components/pages/device/axis/Axes.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import AxisActions from '@renderer/components/pages/device/axis/AxisActions.vue'
+import { useDeviceStore } from '@renderer/stores/device.store'
+import { computed } from 'vue'
+
+const store = useDeviceStore()
+
+const bindings = computed(() => store.currentBindings)
+
+function removeAxis(axis: string): void {
+  store.removeAxisBinding(axis)
+}
+</script>
+
+<template>
+  <div class="device-setup-actions">
+    <template v-for="(_, axis) in bindings.axes" :key="axis">
+      <hr />
+
+      <AxisActions :axis="axis as string" @remove="removeAxis(axis as string)" />
+    </template>
+  </div>
+</template>
