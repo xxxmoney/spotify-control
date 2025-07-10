0a9f28a44cd895fb35b05c4bf6822ba77a65917c
xxxmoney <xxxmoney111@gmail.com>
2025-07-10 17:22:27 +0200

    Improvements, tidying up

--- src/renderer/src/helpers/handler.helper.ts
+++ src/renderer/src/helpers/handler.helper.ts
@@ -1,73 +1,102 @@
-import { DeviceBindings, DeviceThumbs, DeviceTriggers } from '@/shared/types'
+import {
+  AxisAction,
+  ButtonAction,
+  DeviceBindings,
+  DeviceThumbs,
+  DeviceTriggers
+} from '@/shared/types'
 import { AxisActionTypeEnum, ButtonActionTypeEnum } from '@renderer/enums/device.enums'
 import { AXES } from '@renderer/constants/device.constants'
 
 export async function handleButtons(buttons: string[], binding: DeviceBindings): Promise<void> {
+  const promises: Promise<void>[] = []
+
   for (const button of buttons) {
     const buttonActions = binding.buttons[button]
 
     if (buttonActions) {
-      for (const action of buttonActions) {
-        console.log(`Action '${action.type}' triggered for button ${button}`)
-
-        switch (action.type) {
-          case ButtonActionTypeEnum.VolumeUp:
-            // TODO: handle volume up action
-            break
-          case ButtonActionTypeEnum.VolumeDown:
-            // TODO: handle volume down action
-            break
-          default:
-            console.warn(`Unknown action type: '${action.type}' for button ${button}`)
-            break
-        }
-      }
+      promises.push(handleButtonActions(buttonActions, button))
     }
   }
+
+  await Promise.all(promises)
 }
 
 export async function handleTriggers(
   triggers: DeviceTriggers,
   binding: DeviceBindings
 ): Promise<void> {
-  let trigger: string | null = null
+  const promises: Promise<void>[] = []
+
   if (triggers.left) {
-    trigger = AXES.LEFT_TRIGGER
+    const axis = AXES.LEFT_TRIGGER
+    const actions = binding.axes[axis]
+    if (actions) {
+      promises.push(handleAxisActions(actions, axis))
+    }
   }
   if (triggers.right) {
-    trigger = AXES.RIGHT_TRIGGER
+    const axis = AXES.RIGHT_TRIGGER
+    const actions = binding.axes[axis]
+    if (actions) {
+      promises.push(handleAxisActions(actions, axis))
+    }
   }
 
-  return await handleAxis(trigger, binding)
+  await Promise.all(promises)
 }
 
 export async function handleThumbs(thumbs: DeviceThumbs, binding: DeviceBindings): Promise<void> {
-  let thumb: string | null = null
+  const promises: Promise<void>[] = []
+
   if (thumbs.left) {
-    thumb = AXES.LEFT_X
+    const axis = AXES.LEFT_X
+    const actions = binding.axes[axis]
+    if (actions) {
+      promises.push(handleAxisActions(actions, axis))
+    }
   }
   if (thumbs.right) {
-    thumb = AXES.RIGHT_X
+    const axis = AXES.RIGHT_X
+    const actions = binding.axes[axis]
+    if (actions) {
+      promises.push(handleAxisActions(actions, axis))
+    }
   }
 
-  return await handleAxis(thumb, binding)
+  await Promise.all(promises)
 }
 
-async function handleAxis(axis: string | null, binding: DeviceBindings): Promise<void> {
-  if (axis) {
-    const triggerActions = binding.axes['XINPUT_GAMEPAD_LEFT_TRIGGER']
-    if (triggerActions) {
-      for (const action of triggerActions) {
-        switch (action.type) {
-          case AxisActionTypeEnum.Volume:
-            console.log(`Action '${action.type}' triggered for left trigger`)
-            // TODO: handle volume action
-            break
-          default:
-            console.warn(`Unknown action type: '${action.type}' for trigger ${axis}`)
-            break
-        }
+async function handleButtonActions(buttonActions: ButtonAction[], button: string): Promise<void> {
+  if (buttonActions) {
+    for (const action of buttonActions) {
+      console.log(`Action '${action.type}' triggered for button ${button}`)
+
+      switch (action.type) {
+        case ButtonActionTypeEnum.VolumeUp:
+          // TODO: handle volume up action
+          break
+        case ButtonActionTypeEnum.VolumeDown:
+          // TODO: handle volume down action
+          break
+        default:
+          console.warn(`Unknown action type: '${action.type}' for button ${button}`)
+          break
       }
+    }
+  }
+}
+
+async function handleAxisActions(axisActions: AxisAction[], axis: string): Promise<void> {
+  for (const action of axisActions) {
+    switch (action.type) {
+      case AxisActionTypeEnum.Volume:
+        console.log(`Action '${action.type}' triggered for left trigger`)
+        // TODO: handle volume action
+        break
+      default:
+        console.warn(`Unknown action type: '${action.type}' for trigger ${axis}`)
+        break
     }
   }
 }
