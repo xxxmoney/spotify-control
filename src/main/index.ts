bf8e59dc6b45796d08e523e3ee2c1d39953c404e
xxxmoney <xxxmoney111@gmail.com>
2025-07-11 19:18:26 +0200

    Remove non functional attempt to handle protocol

--- src/main/index.ts
+++ src/main/index.ts
@@ -86,22 +86,6 @@ app.whenReady().then(async () => {
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
 
-  // Handle protocols
-  // protocol.handle(constants.SPOTIFY_PROTOCOL, (request) => {
-  //   const raw_code = /access_token=([^&]*)/.exec(request.url) || null
-  //   const token = raw_code && raw_code.length > 1 ? raw_code[1] : null
-  //
-  //   if (token) {
-  //     console.log('Token captured in main process:', token)
-  //
-  //     // TODO: send token to renderer process or store it as needed
-  //   }
-  //
-  //   return new Response('<h1>Success</h1>', {
-  //     headers: { 'content-type': 'text/html' }
-  //   })
-  // })
-
   createWindow()
   await installExtensions()
 })
