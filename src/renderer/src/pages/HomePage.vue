2ad9ba779e6f4ffa4a247aee06fba50624fc07dc
xxxmoney <xxxmoney111@gmail.com>
2025-07-12 13:37:44 +0200

    WIP spotify store and usage of web api to authorise

--- src/renderer/src/pages/HomePage.vue
+++ src/renderer/src/pages/HomePage.vue
@@ -1,20 +1,37 @@
 <script setup lang="ts">
 import * as routeConstants from '@renderer/constants/route.constants'
 import { useRouter } from 'vue-router'
+import { useSpotifyStore } from '@renderer/stores/spotify.store'
+import { computed, onMounted } from 'vue'
 
 const router = useRouter()
+
+const spotifyStore = useSpotifyStore()
+
+const isAuthorised = computed(() => spotifyStore.isAuthorised)
 
 async function goToDevices(): Promise<void> {
   await router.push({ name: routeConstants.DEVICES })
 }
 
-// TODO: on mounted do authorise - add to store, store token, create instance of spotify-web-api-js
+onMounted(async () => {
+  if (!isAuthorised.value) {
+    await spotifyStore.authorise()
+
+    // TODO: wait somehow for callback with authentication - have to somehow get it from main
+  }
+})
 </script>
 
 <template>
   <div class="home-page">
     <div class="title">Spotify Control</div>
 
-    <button class="action" @click="goToDevices">Show devices</button>
+    <template v-if="!isAuthorised">
+      <p>Please authorise in Spotify to continue</p>
+    </template>
+    <template v-else>
+      <button class="action" @click="goToDevices">Show devices</button>
+    </template>
   </div>
 </template>
---
+++ src/renderer/src/stores/spotify.store.ts
@@ -0,0 +1,34 @@
+import { ref } from 'vue'
+import { defineStore } from 'pinia'
+import * as webApi from '@renderer/api/spotify.api'
+import SpotifyWebApi from 'spotify-web-api-js'
+import { SPOTIFY_SCOPES } from '@/shared/constants'
+
+export const useSpotifyStore = defineStore('counter', () => {
+  const controlApi = new SpotifyWebApi()
+
+  const isAuthorised = ref(false)
+  const token = ref<string | null>(null)
+
+  function setToken(newToken: string): void {
+    token.value = newToken
+    controlApi.setAccessToken(newToken)
+  }
+
+  async function authorise(): Promise<void> {
+    await webApi.authorise({
+      clientId: null,
+      redirectUri: null,
+      scope: SPOTIFY_SCOPES.join(' ')
+    })
+  }
+
+  return {
+    api: controlApi,
+
+    isAuthorised,
+
+    authorise,
+    setToken
+  }
+})
