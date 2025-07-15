54754c116609105f17c466778dcf36c1856728e1
xxxmoney <xxxmoney111@gmail.com>
2025-07-15 18:21:41 +0200

    Fixes for fetch token

--- src/main/handlers/spotify.handler.ts
+++ src/main/handlers/spotify.handler.ts
@@ -12,15 +12,23 @@ export async function handleSpotifyAuthCallback(params: { [p: string]: string })
 }
 
 async function fetchToken(code: string): Promise<string> {
-  const body = {
+  const body = new URLSearchParams({
     grant_type: constants.SPOTIFY_GRANT_TYPE,
     code: code,
     redirect_uri: constants.SPOTIFY_REDIRECT_URL
-  }
+  })
+
+  const authorizationBase64 = Buffer.from(
+    process.env.SPOTIFY_CLIENT_ID + ':' + process.env.SPOTIFY_CLIENT_SECRET
+  ).toString('base64')
 
   const response = await fetch(constants.SPOTIFY_TOKEN_URL, {
     method: 'POST',
-    body: JSON.stringify(body)
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      Authorization: `Basic ${authorizationBase64}`
+    },
+    body: body
   })
 
   if (!response.ok) {
